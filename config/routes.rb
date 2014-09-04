4cb75838e251e4912ad38455bd0c6d10967b0746
Chris Elwood <kudzooman@gmail.com>
2014-09-04 06:27:41 -0700

    Updated app layout and welcome views.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   root to: 'welcome#index'
+  # root is a method that takes a hash as an argument, here using an "implied hash" syntax. 
+  # The line could be rewritten as: root({to: 'welcome#index'}). 
+  # You'll see implied hashes frequently in Rails.
 end
